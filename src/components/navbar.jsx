8174d31cc6fe45e5be267779d85c30e8193841d9
chris jhovin <chrisjhovincabral26@yahoo.com>
2023-10-23 19:11:58 +0800

    update

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+<img src="" alt="" />
+<div className='desktopMenu'></div>
+
+    </nav>
+  )
+}
+
+export default Navbar
