af1df12e1b3a74934b8e464b30060c9dd5154729
chris jhovin <chrisjhovincabral26@yahoo.com>
2023-10-25 21:57:47 +0800

    update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './navbar.css';
 import logo from '../assets/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -17,8 +17,8 @@ const Navbar = () => {
       <Link className='desktopMenuListItem' >Contact</Link>
       </div>
       <div>
-        <FontAwesomeIcon className='icons' icon={faFacebook} />
-        <FontAwesomeIcon className='icons' icon={faInstagram} />
+      <FontAwesomeIcon className='icons' icon={faGithub} />
+        <FontAwesomeIcon className='icons' icon={faPhone} />
       </div>
     </nav>
   )
