d525be2e6921aa91b1aef14ea1bf342476d50d1c
chris jhovin <chrisjhovincabral26@yahoo.com>
2023-10-28 22:45:46 +0800

    update

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -3,12 +3,25 @@ import './intro.css'
 import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faUserTie } from '@fortawesome/free-solid-svg-icons'
 import bg from '../assets/redneon.gif';
 import mypic from '../assets/edited.png';
 import Typewriter from "typewriter-effect";
 
 
+const MyCv_FILE_URL = 'http://localhost:3000/MyCv.docx';
+
 const Intro = () => {
+
+    const downloadFileAtURL = (url) => {
+        const fileName = url.split('/').pop()
+        const aTag = document.createElement('a')
+        aTag.href = url
+        aTag.setAttribute('download', fileName)
+        document.body.appendChild(aTag)
+        aTag.click();
+        aTag.remove();
+    }
 
     const [state] = useState({
         title: "Hi, ",
@@ -37,7 +50,7 @@ const Intro = () => {
                 </div>
                 <div className='contact-me'>
                     <button className='button'><FontAwesomeIcon icon={faBriefcase} /> Hire Me!</button>
-
+                    <button className='button' onClick={() => { downloadFileAtURL(MyCv_FILE_URL) }}><FontAwesomeIcon icon={faUserTie} /> Download CV</button>
                 </div>
 
             </div>
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -8,16 +8,37 @@ import '../components/skills.css';
 export const skills = () => {
     return (
         <section className='skills'>
-            <span className='skillTitle'>What I do</span>
+            <span className='skillTitle'>What I do <br></br></span>
             <span className='skillDesc'>I'am skilled and passionate web
                 designer with experience in creating visually appealing and user-friendly wensite. I have a strong understanding of design, and sensitive eye for detail.
             </span>
             <div className="skillBars">
-                <div className="skillBar"><div className="img skillBarImg"></div></div>
+                <div className="skillBar">
+                    <img src={UiImg} alt="" className='skillBarImg' />
+                    <div className='skillBarText'>
+                        <h2>UI/UX Design</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={WebDes} alt="" className='skillBarImg' />
+                    <div className='skillBarText'>
+                        <h2>website developer</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={WriteCode} alt="" className='skillBarImg' />
+                    <div className='skillBarText'>
+                        <h2>Writing code</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+
             </div>
 
 
         </section>
-    )
+    )   
 }
 export default skills
