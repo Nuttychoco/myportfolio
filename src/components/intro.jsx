68188c6e10eea03db34e64afe28b8cd12dd4e420
chris jhovin <chrisjhovincabral26@yahoo.com>
2023-10-29 16:02:40 +0800

    update

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -9,7 +9,7 @@ import mypic from '../assets/edited.png';
 import Typewriter from "typewriter-effect";
 
 
-const MyCv_FILE_URL = 'http://localhost:3000/MyCv.docx&chrisjhovincabral.com/MyCv.docx';
+const MyCv_FILE_URL = 'chrisjhovincabral.com/MyCv.docx';
 
 const Intro = () => {
 
