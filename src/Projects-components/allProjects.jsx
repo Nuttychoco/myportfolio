7197fe23257ded2fad5a6fb3b0c79eefa91623a3
chris jhovin <chrisjhovincabral26@yahoo.com>
2023-11-06 06:33:14 +0800

    update

--- src/Projects-components/allProjects.jsx
+++ src/Projects-components/allProjects.jsx
@@ -4,14 +4,24 @@ import Kndt from '../assets/Kndt.PNG'
 
 export const AllProjects = () => {
   return (
-  <section className='AllProjects'>
-<div className='recentProject'>
-    <div className='project1'>
-    <img src={Kndt} alt="" className='projectimg1'/>
-    </div>
-</div>
+    <section className='AllProjects'>
+      <div className='recentProject'>
+        <div className='project1'>
+          <div>
+            <img src={Kndt} alt="" className='projectimg1' />
+          </div>
+          <div className='projectGroupText'>
+            <h1 className='imgTitle'>KNDT Arabia</h1>
+            <div className='suppText1'>Inventory App </div>
+            <div className='desText'>Static, Multi-page Application and SQL driven.</div>
+            <div className=''>Tech Used: </div>
+            <div></div>
+          </div>
 
-  </section>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
