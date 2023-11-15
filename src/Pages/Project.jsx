7591fa7a978e36736a498fbf254aed641e03f026
chris <chrisjhovincabral25@yahoo.com>
2023-11-15 18:05:45 +0800

    update

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -6,13 +6,10 @@ export const Project = () => {
   return (
     <section className='projectPage'>
       <div className='projectItem'>
-        <div className='boxShadow'>
           <div className='title2'>
             <h2 className="projectTitle"><span className="yellowText">Recent</span>Project</h2>
           </div>
           <AllProjects />
-
-        </div>
       </div>
     </section>
   )
